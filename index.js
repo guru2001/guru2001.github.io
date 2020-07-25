2057e267359fac986546ad5d6b8f6eee53385ce8
guru2001 <ramaguruprakash@gmail.com>
2020-07-25 20:16:14 +0530

    added text

---
+++ index.js
@@ -0,0 +1,11 @@
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  });
